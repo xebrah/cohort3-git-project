452a5affe9b3beef14972babb40f6003b11448a1
Ibrahim Shafiq <ibrahimshafiq104@gmail.com>
2025-08-05 20:11:08 +0300

    Initial commit

---
+++ converter.py
@@ -0,0 +1,9 @@
+# define the converter func
+def celcius_to_fahrenheit(celcius):
+    """
+    Convert Celsius to Fahrenheit.
+    
+    :param celcius: Temperature in Celsius
+    :return: Temperature in Fahrenheit
+    """
+    return (celcius * 9/5) + 32
